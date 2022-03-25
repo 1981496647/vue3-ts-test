34f68f3fa7f8bc0839629f9b5671902cafbf9b0c
1981496647 <1981496647@qq.com>
2022-03-25 14:20:20 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author       : 林青云
+ * @Date         : 2022-03-25 13:32:59
+ * @LastEditors  : 林青云
+ * @LastEditTime : 2022-03-25 14:11:11
+ * @Description  : file content
+ * @FilePath     : \vue3-ts-test\src\App.vue
+-->
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
