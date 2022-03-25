5f25c145c2ec433836c83960fbfadb77e0f2b91a
1981496647 <1981496647@qq.com>
2022-03-25 14:49:35 +0800

    cs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author       : 林青云
+ * @Date         : 2022-03-25 13:32:59
+ * @LastEditors  : 林青云
+ * @LastEditTime : 2022-03-25 14:30:03
+ * @Description  : file content
+ * @FilePath     : \vue3-ts-test\src\views\AboutView.vue
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
