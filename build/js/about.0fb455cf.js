d9656485663a01d15e2ad8e8ebf0b22031c93924
1981496647 <1981496647@qq.com>
2022-03-25 15:17:12 +0800

    tijiao

---
+++ build/js/about.0fb455cf.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue3_ts_test"]=self["webpackChunkvue3_ts_test"]||[]).push([[443],{4018:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var s=n(3396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function c(t,e){return(0,s.wg)(),(0,s.iD)("div",u,r)}var i=n(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.0fb455cf.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
+/*
+ * @Author       : 林青云
+ * @Date         : 2022-03-25 14:50:10
+ * @LastEditors  : 林青云
+ * @LastEditTime : 2022-03-25 15:13:35
+ * @Description  : file content
+ * @FilePath     : \vue3-ts-test\vue.config.js
+ */
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: './build',
+  // configureWebpack: {
+  //   resolve: {
+  //     alias :{
+  //       components: '@/component'
+  //     }
+  //   }
+  // }
+  configureWebpack: (conifg) => {
+    conifg.resolve.alias = {
+      '@': path.resolve(__dirname, 'src'),
+      components: '@/components',
+      view: '@/views'
+    }
+  }
 })
